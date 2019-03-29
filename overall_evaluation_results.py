4a0a8a25ace8afb8b8a7ab3faf8cc1b079e74fb1
Mahmut Ozge Karakaya <mokarakaya@gmail.com>
2019-03-30 00:18:48 +0100

    add overall results calculator

---
+++ overall_evaluation_results.py
@@ -0,0 +1,32 @@
+import csv
+
+
+def generate_overall_results():
+    datasets = ['Movielens1M', 'yoochoose_buys']
+    cell_names = ['BasicRNNCell', 'LSTMCell', 'GRUCell', 'DBAM', 'LSTMCellDiversity']
+    metrics = ['recall', 'diversity', 'aggregate_diversity', 'unexpectedness', 'novelty']
+    with open('overall_evaluation_results.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        for dataset in datasets:
+            writer.writerow([dataset])
+            metrics_line = ['metric']
+            metrics_line.extend(cell_names)
+            writer.writerow(metrics_line)
+            results = {}
+            for cell_name in cell_names:
+                path = dataset + '/' + cell_name + '.evaluation_results.csv'
+                reader = csv.reader(open(path))
+                for line in reader:
+                    key, value = line
+                    if key not in results:
+                        results[key] = []
+                    results[key].append(value)
+            for metric in metrics:
+                results_line = [metric]
+                results_line.extend(results[metric])
+                writer.writerow(results_line)
+            writer.writerow('')
+
+
+generate_overall_results()
+
