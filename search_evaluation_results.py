5c1c2f4cde14004e61a9709ce11674248c3a9238
mokarakaya <mahmut.karakaya@ing.com>
2019-12-06 22:01:31 +0100

    add search evaluation results

--- search_evaluation_results.py
+++ search_evaluation_results.py
@@ -10,32 +10,25 @@ def generate_overall_results():
     # cell_names = ['BasicRNNCell', 'LSTMCell', 'GRUCell', 'GRU4Rec', 'DBAM', 'Baseline', 'ImplicitSequenceModel']
     cell_name = 'LSTMCellDiversity'
     # cell_names = ['Baseline']
-    search_space = [2, 3]
+    search_space = [2, 4, 5, 6, 8]
 
     metrics = ['precision', 'recall', 'mrr', 'diversity', 'aggregate_diversity', 'unexpectedness', 'novelty', 'precision_at_one', 'recall_at_one', 'mrr_at_one']
     # metrics = ['precision', 'recall', 'mrr', 'diversity', 'aggregate_diversity', 'unexpectedness', 'novelty']
-    with open('overall_evaluation_results.csv', 'w') as csvfile:
-        writer = csv.writer(csvfile)
-        for dataset in datasets:
-            writer.writerow([dataset])
-            metrics_line = ['metric']
-            metrics_line.extend(cell_names)
+    for dataset in datasets:
+        with open(dataset + '_' + 'overall_search_evaluation_results.csv', 'w') as csvfile:
+            writer = csv.writer(csvfile)
+            metrics_line = ['key']
+            metrics_line.extend(metrics)
             writer.writerow(metrics_line)
-            results = {}
             for search in search_space:
-                path = dataset + '/' + cell_name + '.evaluation_results.csv'
+                path = dataset + '/' + cell_name + str(search) + '.search_evaluation_results.csv'
                 reader = csv.reader(open(path))
+                results_line = [search]
                 for line in reader:
                     key, value = line
-                    if key not in results:
-                        results[key] = []
-                    results[key].append(value)
-            for metric in metrics:
-                results_line = [metric]
-                results_line.extend(results[metric])
+                    if key in metrics:
+                        results_line.extend([value])
                 writer.writerow(results_line)
-            writer.writerow('')
-
 
 generate_overall_results()
 
